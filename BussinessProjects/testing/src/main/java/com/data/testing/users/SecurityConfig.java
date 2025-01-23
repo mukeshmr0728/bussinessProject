e6f136cc7197b1408d785ee3fae21cfedf0a315a
Ganesh <134919900+kganesh09@users.noreply.github.com>
2025-01-23 18:55:02 +0530

    hiii
    fcghjk

--- BussinessProjects/testing/src/main/java/com/data/testing/users/SecurityConfig.java
+++ BussinessProjects/testing/src/main/java/com/data/testing/users/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.data.testing.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class SecurityConfig {
 
     @Autowired
     private UserService userService;
+
+    public PasswordEncoder passwordEncoder(){
+        return passwordEncoder();
+    }
 }
